aab6a46ad03666e8987c3ad4c036af5301191734
hammadryaz <hammadryaz@hotmail.com>
2026-03-05 09:45:00 +0500

    c++ 10th 11th program

---
+++ c++/10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+/*
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
+int main()
+{
+    int count = 5;
+    for (size_t i = 1; i <= count; i++)
+    {
+        for (size_t space = 1; space <= count - i; space++)
+        {
+            cout << " ";
+        }
+        for (size_t star = 1; star <= (2 * i) - 1; star++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ c++/11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+int main()
+{
+    int count = 5;
+    for (size_t i = count; i >= 1; i--)
+    {
+        for (size_t space = 1; space <= count - i; space++)
+        {
+            cout << " ";
+        }
+        for (size_t star = 2 * i - 1; star >= 1; star--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
