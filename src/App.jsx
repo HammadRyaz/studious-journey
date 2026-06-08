e072b449f0f02dfe84ee9afb94f4c8b7fe7233ff
hammadryaz <hammadriaz.me@gmail.com>
2026-06-09 02:33:05 +0500

    diff in event.currentTarget & event.target

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import Header from './Header';
 const App = () => {
   const ingredients = ["Chicken", "Oregano", "Tomatoes"]
   const ingredientsListItem = ingredients.map(i => <li>{i}</li>)
+  function handleSubmit(event) {
+    event.preventDefault();
+    // const fromData = new FormData(event)
 
+  }
+  function handleClick(event) {
+    console.log("currentTarget")
+    console.log(event.currentTarget);
+    console.log("Target")
+    console.log(event.target);
+  }
   return (
     <>
       <Header />
       <main>
-        <form className="add-ingredient-form">
+        <form className="add-ingredient-form"
+          aria-label='Add Ingredients'
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add ingredient"
           />
           <button>Add ingredient</button>
+          <button onClick={handleClick}> <span>Event</span></button>
+
         </form>
         <ul className='py-2'>
           {ingredientsListItem}
