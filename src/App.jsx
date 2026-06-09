9ba88e12cb9546895de31b497a9db783414fbc62
hammadryaz <hammadriaz.me@gmail.com>
2026-06-09 05:07:46 +0500

    fvrt things task done

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import Header from './Header';
 
 const App = () => {
   const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-  const ingredientsListItem = ingredients.map(i => <li>{i}</li>)
+  const [fvrtThings, setFvrtThings] = React.useState(["💦🌹"]);
+  const ingredientsListItem = ingredients.map(i => <li key={i}>{i}</li>)
   function handleSubmit(event) {
     event.preventDefault();
-    // const fromData = new FormData(event)
-
+    console.log("clicked")
+    const formData = new FormData(event.currentTarget)
+    const newIngredients = formData.get("ingredient")
+    ingredients.push(newIngredients)
+    console.log(ingredients)
   }
-  function handleClick(event) {
-    console.log("currentTarget")
-    console.log(event.currentTarget);
-    console.log("Target")
-    console.log(event.target);
+  const allFavoriteThings = ["😺", "💡🫖", "🔥🧤", "🟤🎁",
+    "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+
+  const displayFvrtThings = fvrtThings.map((f) => <li key={f}>{f}</li>)
+
+  function handFvrtThings(e) {
+    e.preventDefault()
+    setFvrtThings(prevThings => {
+      if (prevThings.length <= allFavoriteThings.length) {
+        console.log(prevThings.length)
+        return [...prevThings, allFavoriteThings[prevThings.length - 1]]
+      } else {
+        console.log("No more things to add")
+        return prevThings
+      }
+    })
   }
   return (
     <>
@@ -21,19 +36,20 @@ const App = () => {
       <main>
         <form className="add-ingredient-form"
           aria-label='Add Ingredients'
-          onSubmit={handleSubmit}
+        // onSubmit={handleSubmit}
         >
           <input
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add ingredient"
+            name='ingredient'
           />
           <button>Add ingredient</button>
-          <button onClick={handleClick}> <span>Event</span></button>
-
+          <button onClick={handFvrtThings} >Add Fvrt Things</button>
         </form>
         <ul className='py-2'>
           {ingredientsListItem}
+          {displayFvrtThings}
         </ul>
       </main>
     </>
