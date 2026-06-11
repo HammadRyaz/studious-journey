877b999ace6a63feb4f37e4997effb91a06d3911
hammadryaz <hammadriaz.me@gmail.com>
2026-06-11 19:06:30 +0500

    saving changes due to electricity issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,24 @@ import React from 'react'
 import Header from './Header';
 
 const App = () => {
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-  const [fvrtThings, setFvrtThings] = React.useState(["💦🌹"]);
+  const [ingredients, setIngredients] = React.useState(["Chicken", "Oregano", "Tomatoes"])
   const ingredientsListItem = ingredients.map(i => <li key={i}>{i}</li>)
   function handleSubmit(event) {
     event.preventDefault();
     console.log("clicked")
     const formData = new FormData(event.currentTarget)
     const newIngredients = formData.get("ingredient")
-    ingredients.push(newIngredients)
-    console.log(ingredients)
-  }
-  const allFavoriteThings = ["😺", "💡🫖", "🔥🧤", "🟤🎁",
-    "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+    setIngredients(prev => [...prev, newIngredients])
 
-  const displayFvrtThings = fvrtThings.map((f) => <li key={f}>{f}</li>)
-
-  function handFvrtThings(e) {
-    e.preventDefault()
-    setFvrtThings(prevThings => {
-      if (prevThings.length <= allFavoriteThings.length) {
-        console.log(prevThings.length)
-        return [...prevThings, allFavoriteThings[prevThings.length - 1]]
-      } else {
-        console.log("No more things to add")
-        return prevThings
-      }
-    })
   }
+
   return (
     <>
       <Header />
       <main>
         <form className="add-ingredient-form"
           aria-label='Add Ingredients'
-        // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
         >
           <input
             type="text"
@@ -45,11 +28,9 @@ const App = () => {
             name='ingredient'
           />
           <button>Add ingredient</button>
-          <button onClick={handFvrtThings} >Add Fvrt Things</button>
         </form>
         <ul className='py-2'>
           {ingredientsListItem}
-          {displayFvrtThings}
         </ul>
       </main>
     </>
