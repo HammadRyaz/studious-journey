e52cf2230a62f849bccc24f52719221d18e0b8c0
hammadryaz <hammadriaz.me@gmail.com>
2026-06-22 21:54:30 +0500

    Add React Buddy integration with component previews and initial hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,2 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev/index.js";
