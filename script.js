efa1203f2211bdc5275f5b2b80a68d43aa4ffc15
hammadryaz <hammadriaz.me@gmail.com>
2026-07-05 17:58:46 +0500

    Deep Copy with fucntion

--- script.js
+++ script.js
@@ -30,17 +30,42 @@
 
 
 //  Deep Copy
-const player = {
-    name: 'Dave',
-    score: 0,
-    incrementScore: function(){
-        this.score++   
-    },
-    skills : ['html','css','js']
-}
-const player2 = structuredClone(player)
-player2.name = "Hammad";
-player2.skills[0] = "react";
+// const player = {
+//     name: 'Dave',
+//     score: 0,
+//     incrementScore: function(){
+//         this.score++   
+//     },
+//     skills : ['html','css','js']
+// }
+// const player2 = structuredClone(player)
+// player2.name = "Hammad";
+// player2.skills[0] = "react";
 
-console.log("1 :- " ,player)
-console.log("2 :- " ,player2)
+// console.log("1 :- " ,player)
+// console.log("2 :- " ,player2)
+
+
+
+//  Deep Copy with fucntion
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
+        this.skills = ["html", "css", "js"];
+    }
+
+    incrementScore() {
+        this.score++;
+    }
+}
+const player = new Player("Dave");
+player.incrementScore()
+
+const copy = structuredClone(player);
+Object.setPrototypeOf(copy , P)
+copy.incrementScore()
+
+console.log(player)
+console.log(copy)
+console.log(copy instanceof Player); 
