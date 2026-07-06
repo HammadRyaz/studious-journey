0725463821feaa52ef9b8c9cd67645a09c2edc64
hammadryaz <hammadriaz.me@gmail.com>
2026-07-06 18:32:36 +0500

    still i use Person.prototype.getDetail.call()  to set function in prototype

--- classes.js
+++ classes.js
@@ -15,24 +15,25 @@ export const classes = () => {
         }
     }
 
-    const u1 = new Person("Hammad", "user")
-    console.log(u1.getDetail())
+
     class Admin extends Person {
         constructor(username, role, auth) {
             super(username, role);
             this.auth = ['delete', 'edit', 'create', 'create-user', 'change-role']
         }
-        getDetail() {
-            const personGetDetail = super.getDetail()
-            return {
-                name: this.username,
-                role: this.role,
-                auth: ['delete', 'edit', 'create', 'create-user', 'change-role']
-            }
+    }
+    Admin.prototype.getDetail = function () {
+        const personGetDetail = Person.prototype.getDetail.call(this)
+        return {
+            ...personGetDetail,
+            auth: ['delete', 'edit', 'create', 'create-user', 'change-role']
         }
     }
 
+
+    const u1 = new Person("Hammad", "user")
+    console.log(u1)
     const a1 = new Admin("Hamster", "admin");
-    console.log(a1.getDetail())
+    console.log(a1)
 }
 classes()
