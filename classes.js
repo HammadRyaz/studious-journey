32046f15e74105826ba23c85d470fd46eec02ae1
hammadryaz <hammadriaz.me@gmail.com>
2026-07-07 16:57:55 +0500

    Private Feild

--- classes.js
+++ classes.js
@@ -1,40 +1,55 @@
 
 export const classes = () => {
 
-    class Person {
-        constructor(username, role) {
-            this.username = username;
-            this.role = role;
+    // class Person {
+    //     constructor(username, role) {
+    //         this.username = username;
+    //         this.role = role;
+    //     }
+
+    // }
+    // Person.prototype.getDetail = function () {
+    //     return {
+    //         name: this.username,
+    //         role: this.role
+    //     }
+    // }
+
+
+    // class Admin extends Person {
+    //     constructor(username, role, auth) {
+    //         super(username, role);
+    //         this.auth = ['delete', 'edit', 'create', 'create-user', 'change-role']
+    //     }
+    // }
+    // Admin.prototype.getDetail = function () {
+    //     const personGetDetail = Person.prototype.getDetail.call(this)
+    //     return {
+    //         ...personGetDetail,
+    //         auth: ['delete', 'edit', 'create', 'create-user', 'change-role']
+    //     }
+    // }
+
+
+    // const u1 = new Person("Hammad", "user")
+    // console.log(u1)
+    // const a1 = new Admin("Hamster", "admin");
+
+    // console.log(a1)
+
+    // Private ----------------------
+    class Holiday {
+        #destination = "lahore"
+        constructor(destination, price) {
+            this.#destination = destination
+            this.price = price
         }
-
-    }
-    Person.prototype.getDetail = function () {
-        return {
-            name: this.username,
-            role: this.role
-        }
     }
 
-
-    class Admin extends Person {
-        constructor(username, role, auth) {
-            super(username, role);
-            this.auth = ['delete', 'edit', 'create', 'create-user', 'change-role']
-        }
-    }
-    Admin.prototype.getDetail = function () {
-        const personGetDetail = Person.prototype.getDetail.call(this)
-        return {
-            ...personGetDetail,
-            auth: ['delete', 'edit', 'create', 'create-user', 'change-role']
-        }
-    }
+    const safari = new Holiday('Kenya', 1000)
+    console.log(safari)
 
 
-    const u1 = new Person("Hammad", "user")
-    console.log(u1)
-    const a1 = new Admin("Hamster", "admin");
 
-    console.log(a1)
 }
 classes()
