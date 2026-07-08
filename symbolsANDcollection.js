30d189cdde5e6de81b144a5b0d7bd034b3bb7791
hammadryaz <hammadriaz.me@gmail.com>
2026-07-08 19:35:29 +0500

    Diff in Obj and Map , with and wihtout example

--- symbolsANDcollection.js
+++ symbolsANDcollection.js
@@ -1,6 +1,4 @@
 export const symbols = () => {
-
-
     // const secret = Symbol("HSKJAD-221BN803-HDBW92");
 
     // const user = {
@@ -10,14 +8,13 @@ export const symbols = () => {
     // }
     // console.log(user[secret])
 
-
     /*
- Challenge:
-     1. Add a hidden property to the book object 
-        holding a librarian’s note to say the book 
-        has gone missing. 
-     2. Log out the new property.
- */
+   Challenge:
+       1. Add a hidden property to the book object 
+          holding a librarian’s note to say the book 
+          has gone missing. 
+       2. Log out the new property.
+   */
     // const book = {
     //     title: "The Catcher in the Rye",
     //     author: "J.D. Salinger",
@@ -27,20 +24,37 @@ export const symbols = () => {
     // book[librarianNote] = "the book has gone missing. "
     // console.log(book[librarianNote])
 
+    // const obj = {};
+    // const key1 = { id: 1 };
+    // const key2 = { id: 2 };
+    // obj[key1] = "first user";
+    // obj[key2] = "second user";
+    // console.log(obj);
 
+    // const usrMap = new Map(); // Map in JS
+    // usrMap.set(key1, "first")
+    // usrMap.set(key2, "second")
+    // console.log(usrMap)
 
+    const userObj = {};
+    userObj["name"] = "Hammad";
+    userObj[1] = "one"; // key auto string ban jayegi
+    userObj[true] = "yes"; // ye bhi string ban jayegi
 
-    const obj = {};
-    const key1 = { id: 1 };
-    const key2 = { id: 2 };
-    obj[key1] = "first user";
-    obj[key2] = "second user";
-    console.log(obj);
+    console.log(Object.entries(userObj)); // sb kuch aik string bn gya hai
+    Object.keys(userObj).forEach((key, val) =>
+        console.log("key : ", key, ": ", typeof key),
+    );
 
-    const usrMap = new Map(); // Map in JS
-    usrMap.set(key1, "first")
-    usrMap.set(key2, "second")
-    console.log(usrMap)
-}
+    const userMap = new Map();
+    userMap.set("name", "Hammad");
+    userMap.set(1, "id");
+    userMap.set(true, "login");
 
-symbols()
+    console.log(userMap);
+    userMap.forEach((val, key) =>
+        console.log("key : ", key, ": ", typeof key)
+    )
+};
+
+symbols();
