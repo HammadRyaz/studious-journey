0b3743318526d84e673ea0137a902b75af7cfc03
hammadryaz <hammadriaz.me@gmail.com>
2026-07-09 01:11:07 +0500

    function* generator example with Slides

--- generator.js
+++ generator.js
@@ -5,9 +5,27 @@ export const generator = () => {
         yield 30
     }
     const fnc = fncGenerator()
-    console.log(fnc.next().value)
-    console.log(fnc.next().value)
-    console.log(fnc.next().value)
+    const slides = [
+        'Slide 1 ',
+        'Slide 2 ',
+        'Slide 3 ',
+        'Slide 4 ',
+    ]
+    function* slideRun(arr) {
+        for (const item of arr) {
+            yield console.log(item)
+        }
+    }
+    const g = slideRun(slides)
+
+    const btn = document.createElement("button")
+    btn.innerText = "Next Slide"
+    document.body.appendChild(btn)
+
+    btn.addEventListener("click", () => {
+        g.next()
+    })
+
 }
 
 generator()
