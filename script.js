13aef054395d93089dd9e426c6b888355ee96a8c
hammadryaz <hammadriaz.me@gmail.com>
2026-07-19 20:48:14 +0500

    res.Header to set header , res.statuscode , and then i get from data local db and check if res.url is /api and res.method is GET to res snd the APi data

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 
 import http from "node:http"
-
+import { getDataFromDB } from "./database/db.js"
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end("Server END", "utf8", () => console.log("Server End"))
+
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+    if (req.url === "/api" && req.method === "GET") {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200;
+        res.end(JSON.stringify(destinations))
+    } else {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 404;
+        res.end(JSON.stringify({
+            error: "not found",
+            message: "The requested route does not exist"
+        }))
+    }
 })
 
 // server.listen(PORT, () => console.log(`Serve listening on port ${PORT}`))
