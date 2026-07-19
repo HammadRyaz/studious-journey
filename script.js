77dbd7cafab3be94fc178a05428919bf91e4bfce
hammadryaz <hammadriaz.me@gmail.com>
2026-07-19 21:48:59 +0500

    challange Done #18-25

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ import http from "node:http"
 import { getDataFromDB } from "./database/db.js"
 const PORT = 8000;
 
-  /*
-  Challenge:
-  1. Check if the url starts with “/api/continent”.
-    (Is there a JS method that allows you to check what a string starts with?)
-
-  2. If it does, serve only items from that continent.
-    (How can you get to what comes after the final slash?)
-    (What method can you use to filter data?)
-  */
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
     if (req.url === "/api" && req.method === "GET") {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200;
         res.end(JSON.stringify(destinations))
-    } else {
+    }
+    else if (req.url.startsWith('/api/continent')) {
+        const parts = req.url.split('/')
+        const continent = parts[3]
+        const continentItem = destinations.filter((data => data.continent.toLowerCase() === continent.toLowerCase()))
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200;
+        res.end(JSON.stringify(continentItem))
+    }
+    else {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 404;
         res.end(JSON.stringify({
