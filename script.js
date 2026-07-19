e72ba2df0cc22115641c7ed14c896afa22a6fd04
hammadryaz <hammadriaz.me@gmail.com>
2026-07-19 23:52:02 +0500

    optimize with utlis function , now i can create many route just like country/china and continent/asia , by using utility function

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-
-import http from "node:http"
-import { getDataFromDB } from "./database/db.js"
+import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
+import sendResponseContent from "./utils/sendResponseContent.js";
+import sendParmsData from "./utils/sendParmsData.js";
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
-    const destinations = await getDataFromDB()
+    const destinations = await getDataFromDB();
     if (req.url === "/api" && req.method === "GET") {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200;
-        res.end(JSON.stringify(destinations))
+        sendResponseContent(res, 200, destinations);
+    } else if (req.url.startsWith("/api/continent")) {
+        const parts = req.url.split("/");
+        const parms = parts[3];
+        const data = sendParmsData(destinations, 'continent', parms)
+        sendResponseContent(res, 200, data);
     }
-    else if (req.url.startsWith('/api/continent')) {
-        const parts = req.url.split('/')
-        const continent = parts[3]
-        const continentItem = destinations.filter((data => data.continent.toLowerCase() === continent.toLowerCase()))
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200;
-        res.end(JSON.stringify(continentItem))
+    else if (req.url.startsWith("/api/country")) {
+        const parts = req.url.split("/");
+        const parms = parts[3];
+        const data = sendParmsData(destinations, 'country', parms)
+        sendResponseContent(res, 200, data);
     }
     else {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 404;
-        res.end(JSON.stringify({
+        sendResponseContent(res, 404, {
             error: "not found",
-            message: "The requested route does not exist"
-        }))
+            message: "The requested route does not exist",
+        });
     }
-})
-
+});
 // server.listen(PORT, () => console.log(`Serve listening on port ${PORT}`))
-server.listen(PORT)
+server.listen(PORT);
