ea1d7b5bd49f6151dce3057ee1b37526afdf7785
hammadryaz <hammadriaz.me@gmail.com>
2026-07-20 00:29:48 +0500

    Aside : Query Parameter

--- utils/sendResponseContent.js
+++
@@ -1,5 +0,0 @@
-export default function sendResponseContent(res, status, payload) {
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = status;
-    res.end(JSON.stringify(payload))
-}
