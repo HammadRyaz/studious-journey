0a9e5c133abe3b9484c2e7e1883873b2c0d22845
hammadryaz <hammadriaz.me@gmail.com>
2026-07-20 00:29:48 +0500

    Aside : Query Parameter

--- utils/sendParmsData.js
+++
@@ -1,5 +0,0 @@
-export default function sendParmsData(data, locationType, locationName) {
-    return data.filter(
-        (find) => find[locationType].toLowerCase() === locationName.toLowerCase()
-    );
-}
