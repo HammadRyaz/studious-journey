01d4f91b033b1e94eff2f36cd6067ead2286c225
hammadryaz <hammadriaz.me@gmail.com>
2026-07-22 13:14:34 +0500

    There is another way : useOther in writeHead method as well | res.writeHead(statusCode,ContentType,AccessControlOrigin,AccessControlMethod)

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ import http from 'node:http'
 
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Method", "GET")
-    res.writeHead("200", { "Content-Type": "text/html" })
+
+    res.writeHead("200", { "Content-Type": "text/html" }, { "access-control-allow-origin": "*" }, { "access-control-allow-method": "GET" })
     res.end("<h1> Server is Running , haha</h1>")
 
 })
