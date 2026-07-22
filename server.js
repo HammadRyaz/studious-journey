709954db2cfbff641c48107b37cadd7dbd5a287d
hammadryaz <hammadriaz.me@gmail.com>
2026-07-22 14:12:31 +0500

    we can also get current dir using CWD(Current Working Directory) process.cwd() its a node method

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import http from 'node:http'
+import path from 'node:path'
 // Scrim : Route and Path
-
+console.clear()
 const PORT = 8000;
-const dir  = import.meta.dirname
-console.log(dir)
+
+// const __dirname = import.meta.dirname
+// const pathPublic = `${__dirname}/public/images`
+// console.log(pathPublic)
+
+console.log(process.cwd())
+
+
 const server = http.createServer((req, res) => {
-    
+
     res.writeHead("200", { "Content-Type": "text/html" }, { "access-control-allow-method": "POST" })
     res.end()
 
