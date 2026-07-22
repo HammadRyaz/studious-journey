2f92cf02fb99c7fc4448bf3fd219d1881562a46a
hammadryaz <hammadriaz.me@gmail.com>
2026-07-22 14:20:51 +0500

    Absolute and Relative Path using the path Module node:path and then path.join(dir,folder,file)-> absolute and path.join(folder,file)->relative

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import http from 'node:http'
 import path from 'node:path'
-// Scrim : Route and Path
+// Scrim : Aside Path Module
 console.clear()
 const PORT = 8000;
 
-// const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname
 // const pathPublic = `${__dirname}/public/images`
 // console.log(pathPublic)
 
-console.log(process.cwd())
+// console.log(process.cwd())
+
+const absolutePath = path.join(__dirname, "public", "index.html")
+// OUTPUT  : /home/user/GITHUB/HR/documentry-the-journey/public/index.html
+const relativePath = path.join("public", "index.html")
+// OUTPUT : public/index.html
 
 
 const server = http.createServer((req, res) => {
