b95cec6e81e67eae869f1a89c5b1f7300656af93
hammadryaz <hammadriaz.me@gmail.com>
2026-07-23 13:31:03 +0500

    the better way to do file system , fs.readFile

--- server.js
+++ server.js
@@ -6,24 +6,18 @@ console.clear()
 const PORT = 8000;
 
 const __dirname = import.meta.dirname
-// const pathPublic = `${__dirname}/public/images`
-// console.log(pathPublic)
-
-// console.log(process.cwd())
-
-const absolutePath = path.join(__dirname, "public", "index.html")
-// OUTPUT  : /home/user/GITHUB/HR/documentry-the-journey/public/index.html
-const relativePath = path.join("public", "index.html")
-// OUTPUT : public/index.html
-
 
 const server = http.createServer((req, res) => {
-    const resoursePath = path.join(__dirname, "public", "index.html")
-    const content = fs.readFileSync(resoursePath, 'utf8')
-
-    res.writeHead("200", { "Content-Type": "text/html" }, { "access-control-allow-method": "POST" })
-    res.end(content)
-
+    const pathToResourse = path.join(__dirname, 'public', 'index.html')
+    fs.readFile(pathToResourse, 'utf8', (err, content) => {
+        if (err) {
+            console.log(err)
+            return
+        } else {
+            res.writeHead(200, {'Content-Type':'text/html'})
+            res.end(content)
+        }
+    })
 })
 
 server.listen(PORT, () => console.log("Server Listening on " + PORT))
