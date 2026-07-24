cade8a9102059e239066f71225143a6ad92aab89
hammadryaz <hammadriaz.me@gmail.com>
2026-07-24 18:13:36 +0500

    Complete the First Node Project with Multiple Route

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-import http from 'node:http'
-import path from 'node:path'
-import fs from 'node:fs'
-// Scrim : Aside FS Module
-console.clear()
+import http from "node:http";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { getContentType } from "./utils/getContentType.js";
+
+console.clear();
 const PORT = 8000;
 
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
-const server = http.createServer((req, res) => {
-    const pathToResourse = path.join(__dirname, 'public', 'index.html')
-    fs.readFile(pathToResourse, 'utf8', (err, content) => {
-        if (err) {
-            console.log(err)
-            return
-        } else {
-            res.writeHead(200, {'Content-Type':'text/html'})
-            res.end(content)
-        }
-    })
-})
+const server = http.createServer(async (req, res) => {
+    const pathToResourse = path.join(
+        __dirname,
+        "public",
+        req.url === "/" ? "index.html" : req.url,
+    );
+    const ext = path.extname(pathToResourse);
+    try {
+        const content = await fs.readFile(pathToResourse);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", getContentType(ext));
+        res.end(content)
+    } catch (err) {
+        console.log(err)
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html')
+        res.end(`
+            <center>
+            <h1> Page Not Found : 404 </h1>
+            </center>
+            `);
+    }
+});
 
-server.listen(PORT, () => console.log("Server Listening on " + PORT))
+server.listen(PORT, () => console.log("Server Listening on " + PORT));
