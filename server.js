a95d3dbddcb26c2726e6c445641e8f3ec2ee59c2
hammadryaz <hammadriaz.me@gmail.com>
2026-07-25 13:52:21 +0500

    created /api route to get sightings.html Post Data using GET method

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import http from 'node:http'
-import { serveStatic } from './utils/serveStatic.js'
+import http from "node:http";
+import { serveStatic } from "./utils/serveStatic.js";
+import { handleGet } from "./handler/handleGet.js";
 
-const PORT = 8000
+const PORT = 8000;
 
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-/*
-Challenge: 
-   1. Set up a route for ‘/api’.
-   2. Nest an if to check if the method is ‘GET’. 
-   2. When a GET request is received to '/api', use handleGet() to handle it.
-*/ 
-    
-    await serveStatic(req, res, __dirname) 
-}) 
+    if (req.url === "/api") {
+        if (req.method === 'GET') {
+            return await handleGet(res)
+        }
+    } else if (!req.url.startsWith("/api")) {
+        await serveStatic(req, res, __dirname);
+    }
+});
 
-server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
- 
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
