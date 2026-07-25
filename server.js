2ef5042cc37fc9f8c6b3bd39275853bbd92b1f9a
hammadryaz <hammadriaz.me@gmail.com>
2026-07-25 13:14:19 +0500

    Scrim : /api Get added

--- server.js
+++ server.js
@@ -1,38 +1,20 @@
-import http from "node:http";
-import path from "node:path";
-import fs from "node:fs/promises";
-import { getContentType } from "./utils/getContentType.js";
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
-console.clear();
-const PORT = 8000;
+const PORT = 8000
 
-const __dirname = import.meta.dirname;
+const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-    const pathToResourse = path.join(
-        __dirname,
-        "public",
-        req.url === "/" ? "index.html" : req.url,
-    );
-    const ext = path.extname(pathToResourse);
-    try {
-        const content = await fs.readFile(pathToResourse);
-        res.statusCode = 200;
-        res.setHeader("Content-Type", getContentType(ext));
-        res.end(content);
-    } catch (err) {
-        if (err.code === "ENOENT") {
-            const content = await fs.readFile(path.join(publicDir, "404.html"));
-            sendResponse(res, 404, "text/html", content);
-        } else {
-            sendResponse(
-                res,
-                500,
-                "text/html",
-                `<html><h1>Server Error: ${err.code}</h1></html>`,
-            );
-        }
-    }
-});
+/*
+Challenge: 
+   1. Set up a route for ‘/api’.
+   2. Nest an if to check if the method is ‘GET’. 
+   2. When a GET request is received to '/api', use handleGet() to handle it.
+*/ 
+    
+    await serveStatic(req, res, __dirname) 
+}) 
 
-server.listen(PORT, () => console.log("Server Listening on " + PORT));
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
+ 
