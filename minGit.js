a4d51c6002c8319cc43beb6d8dcf59f5da0946c9
hammadryaz <hammadriaz.me@gmail.com>
2026-07-27 20:07:56 +0500

    a simple Commit class would have and id to reference it and a change containing the snapshot of change made.

--- minGit.js
+++ minGit.js
@@ -3,3 +3,9 @@
 function Git(name) {
     this.name = name;
 }
+
+// a simple Commit class would have and id to reference it and a change containing the snapshot of change made.
+function Commit(id, message) {
+    this.id = id
+    this.message = message
+}
