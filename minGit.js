849ea3cc06bedc04876a1447dd9470383d50144d
hammadryaz <hammadriaz.me@gmail.com>
2026-07-27 20:15:44 +0500

    id to give to the new commit, the Git class keep track of the commit ids by keeping a counter called lastCommitId

--- minGit.js
+++ minGit.js
@@ -1,17 +1,20 @@
-
-// simple class called Git which will basically represent a repo.
-function Git(name) {
-    this.name = name;
-}
-
 // a simple Commit class would have and id to reference it and a change containing the snapshot of change made.
 function Commit(id, message) {
     this.id = id
     this.message = message
 }
 
+
+
+// simple class called Git which will basically represent a repo.
+function Git(name) {
+    this.name = name;
+    this.lastCommitId = -1
+}
+
+
 // Lets add the ability on our Git class to create a commit or commit (verb)
 Git.prototype.commit = function (message) {
-    var commit = new Commit()
+    var commit = new Commit(++this.lastCommitId,message)
     return commit;
 }
