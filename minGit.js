998d8936366590ed32a8019cadba8610c06cbf29
hammadryaz <hammadriaz.me@gmail.com>
2026-07-27 21:12:03 +0500

    added a property called git log but not compelete , currently target the head without branch

--- minGit.js
+++ minGit.js
@@ -1,5 +1,5 @@
 // a simple Commit class would have and id to reference it and a change containing the snapshot of change made.
-function Commit(id, parent , message) {
+function Commit(id, parent, message) {
     this.id = id
     this.parent = parent
     this.message = message
@@ -17,4 +17,13 @@ Git.prototype.commit = function (message) {
     var commit = new Commit(++this.lastCommitId, message)
     this.HEAD = commit
     return commit;
+}
+Git.prototype.log = function () {
+    var commit = this.HEAD
+    var history = []
+    while (commit) {
+        history.push(commit)
+        commit = commit.parent
+    }
+    return history
 }
