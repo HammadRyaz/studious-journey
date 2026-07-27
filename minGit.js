bc552035b568b79691886a74d60c84542ad1540e
hammadryaz <hammadriaz.me@gmail.com>
2026-07-27 20:52:21 +0500

    added a property called HEAD in the Git class which will reference the last commit's Commit object

--- minGit.js
+++ minGit.js
@@ -1,24 +1,20 @@
 // a simple Commit class would have and id to reference it and a change containing the snapshot of change made.
-function Commit(id, message) {
+function Commit(id, parent , message) {
     this.id = id
+    this.parent = parent
     this.message = message
 }
-
-
 
 // simple class called Git which will basically represent a repo.
 function Git(name) {
     this.name = name;
     this.lastCommitId = -1
+    this.HEAD = null;
 }
-
 
 // Lets add the ability on our Git class to create a commit or commit (verb)
 Git.prototype.commit = function (message) {
     var commit = new Commit(++this.lastCommitId, message)
+    this.HEAD = commit
     return commit;
 }
-
-var repo = new Git("my-repo")
-console.log(repo.commit("Check Commits Works or Not"))
-console.log(repo)
